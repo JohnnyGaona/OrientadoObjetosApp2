29911b967e9b2e2c8dad5e95e563da907fb88005
Johnny Gaona <103288332+JohnnyGaona@users.noreply.github.com>
2022-04-28 15:19:24 +0000

    Mejora de lectura con listas y mostrar la lista

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 import java.util.*;
 
+
 class Main {
    
    
   public static void main(String[] args) {
-  int ans;
+  int ans =0;
   float interes;
   float Saldo = 0;
 	int NCta;
@@ -14,44 +15,54 @@ class Main {
   float Costo;
   Scanner ing = new Scanner (System.in);   
   
-    List <Ahorros> cta = new ArrayList();
-    List <Corrientes> ctc = new ArrayList();
-    Ahorros obj; 
-    Corrientes obj_2;
-    System.out.println("Bienvenido a la banca movil....");
-    System.out.println("Por favor seleccione el tipo de cuenta...");
-    System.out.println("1) Ahorro / 2) Corriente");
-    ans = ing.nextInt();
-    System.out.print("Ingrese nombre del titula: ");
-    Nombre = ing.next();
-    System.out.print("Ingrese cedula del titula: ");
-    Cedula = ing.next();
-    System.out.print("Ingrese numero Cta. del titula: ");
-    NCta = ing.nextInt();
-    
+    List <Cuenta> cta = new ArrayList<Cuenta>();
+
+    Cuenta obj; 
+    do{
+      System.out.println("Bienvenido a la banca movil....");
+      System.out.println("Por favor seleccione el tipo de cuenta...");
+      System.out.println("1) Ahorro / 2) Corriente / 3) Mostrar las cuentas / 4) Salir ");
+        ans = ing.nextInt(); 
+      
+    if (ans != 3){
+      System.out.print("Ingrese nombre del titula: ");
+        Nombre = ing.next();
+      System.out.print("Ingrese cedula del titula: ");
+        Cedula = ing.next();
+      System.out.print("Ingrese numero Cta. del titula: ");
+        NCta = ing.nextInt();
     if(ans==1){
-    System.out.print("Ingrese interes del titula: ");
-    interes = ing.nextFloat();
+      System.out.print("Ingrese interes del titula: ");
+        interes = ing.nextFloat();
       obj = new Ahorros(interes, Saldo, NCta, Cedula, Nombre);              
       cta.add(obj);
-      System.out.println(cta.toString());
-
+              
     }else{
       
       System.out.print("Ingrese costo del titula: ");
-      Costo = ing.nextFloat();
-      obj_2 = new Corrientes(Costo, Saldo, NCta, Cedula, Nombre);
-      ctc.add(obj_2);
-      System.out.println(ctc.toString());
+        Costo = ing.nextFloat();
+      obj = new Corrientes(Costo, Saldo, NCta, Cedula, Nombre);
+      cta.add(obj);
     }
+   }
+
     
-  //Cuenta obj = new Cuenta();
+
+      if(ans == 3){
+      if (!cta.isEmpty()){
+      System.out.println(cta.toString());
+      }else{
+        System.out.println("Aun no hay cuentas.....");
+      }
+    }
+      
+    }while(ans != 4);
+
+
+    
     
   }
   
-    public void leer(){
-
-  }
 
 }
 
